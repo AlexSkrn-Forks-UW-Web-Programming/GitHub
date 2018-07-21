40eb753b6567b7a9aba5b3b3a3f8306c11eb6c06
alexskrn <askrn123@gmail.com>
2018-07-21 21:47:46 +0300

    Initial commit

---
+++ github.py
@@ -0,0 +1,15 @@
+"""Retrieve some info using GitHib API."""
+
+import requests
+
+user = "AlexSkrn"
+
+response = requests.get('https://api.github.com/users/{}/events'.format(user))
+
+event = response.json()[0]['type']
+
+creation_date = response.json()[0]['created_at']
+
+print("The user {}'s latest event was {} on {}".format(user,
+                                                     event,
+                                                     creation_date))
